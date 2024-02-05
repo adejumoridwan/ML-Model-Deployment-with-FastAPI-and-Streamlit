a4f7ceca1a726167254a6495c7b50a643d58f910
Adejumo Ridwan Suleiman <adejumo999@gmail.com>
2024-02-05 20:27:54 +0100

    edit lesson 7

--- 2_fastapi/6_body/main.py
+++ 2_fastapi/6_body/main.py
@@ -1,74 +1,27 @@
-"""
-- You can mix Path, Query and request body parameter freely
-- You can also declare body parameters as optional by setting the default to None:
-"""
-
 from typing import Annotated
 
-from fastapi import FastAPI, Path, Field
-from pydantic import BaseModel
+from fastapi import FastAPI, Path, Body
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Student(BaseModel):
-    name: str
+    name: str = Field(default=None, max_length=20)
     sex: str
-    age: float
+    age: float = Field(ge=0, description="The age of a student", le=20)
     score: float | None = None
 
 
-# starts secondly
-class Lecturer(BaseModel):
-    name: str
-    course: str | None = None
-
-
-@app.put("/students/{student_id}")
+@app.post("/students/{student_id}")
 async def update_student(
-    student_id: Annotated[int, Path(title="The student id", ge=0, le=1000)],
+    student_id: Annotated[int, Path(title="The student id", ge=0, le=100)],
     bmi: float | None = None,
-    student: Student | None = None,
+    student: Annotated[Student | None, Body()] = None,
 ):
     results = {"student_id": student_id}
     if bmi:
-        results.update({"bmi": 25.6})
+        results.update({"bmi": bmi})
     if student:
         results.update({"student": student})
     return results
-
-
-@app.put("/lecturers/{lecturer_id}")
-async def update_lecturer(lecturer_id: int, student: Student, lecturer: Lecturer):
-    results = {
-        "lecturer_id": lecturer_id,
-        "student": student.model_dump(),
-        "lecturer": lecturer.model_dump(),
-    }
-    return results
-
-
-# Singular values in body
-"""
-importance: Annotated[int, Body()]
-"""
-
-# adding validation and metadata to pydantic models
-# field is imported from pydantic and not from fastapi so is other attributes also
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(gt=0, description="The price must be greater than zero")
-    tax: float | None = None
-
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
-    results = {"item_id": item_id, "item": item}
-    return results
-
-# Nested Models
--- 2_fastapi/7_forms_and_files/main.py
+++ 2_fastapi/7_forms_and_files/main.py
@@ -1,75 +1,29 @@
-# Whenever you want to receive a file instead of JSON you use Form
-
 from typing import Annotated
-
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, File, UploadFile
 
 app = FastAPI()
 
 
 @app.post("/login/")
-async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+async def login(
+    username: Annotated[str, Form(max_length=5)], password: Annotated[str, Form()]
+):
     return {"username": username}
 
 
-"""
-- the above is requesting for form fields and not JSON
-- You can do all the validations and alias you did previously for Query, Path and Cookie
-- HTML sends data to a server using form fields
-"""
-
-# Request Files
-"""
-You can define files to be uploaded by the client using File,
-because uploaded files are sent as form data
-"""
-
-from typing import Annotated
-
-from fastapi import FastAPI, File, UploadFile
-
-
 @app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
-
-
-"""
-advantages of Upload file
-- you don't have to use File()
-- work well for large files like images, videos e.t.c
-- you can get the metadata of an uploaded file
-
-
-"""
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
-
-
-# making upload otional
-@app.post("/uploadfiles/")
-async def create_upload_file(file: UploadFile | None = None):
+async def create_file(file: UploadFile):
     if not file:
         return {"message": "No upload file sent"}
     else:
         return {"filename": file.filename}
 
 
-# Defining Files and Forms together
-
-from typing import Annotated
-
-from fastapi import FastAPI, File, Form, UploadFile
-
-
 @app.post("/files-to/")
 async def create_file(
     file: Annotated[bytes, File()],
-    fileb: Annotated[UploadFile, File()],
     token: Annotated[str, Form()],
+    fileb: Annotated[UploadFile, File()] = None,
 ):
     return {
         "file_size": len(file),
