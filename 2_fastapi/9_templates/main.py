27ef3016e886a6ec8c56a7d273cf0410811b9810
Adejumo Ridwan Suleiman <adejumo999@gmail.com>
2024-02-09 15:03:58 +0100

    edit lesson 9

--- 2_fastapi/9_templates/main.py
+++ 2_fastapi/9_templates/main.py
@@ -1,28 +1,27 @@
-"""
-- These are file structures that are used to represent the application 
-user interface, they are separated from the application logic.
-- You can use the Jinja2Templates or any other templating engine of you choice.
-- Install using pip install jinja2
-"""
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-"""
-Mounting means the application mounted is independent of the fast api application and will not reflect on the fastapi docs
-"""
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/students/{student_id}", response_class=HTMLResponse)
-async def read_item(request: Request, student_id: str):
+async def read_student(request: Request, student_id: str):
     return templates.TemplateResponse(
         request=request, name="students.html", context={"student_id": student_id}
     )
+
+
+@app.get("/numbers/", response_class=HTMLResponse)
+async def get_no(request: Request, no: str = 5):
+    return templates.TemplateResponse(
+        request=request, name="no.html", context={"no": no}
+    )
+
+
+# pip install Jinja2
