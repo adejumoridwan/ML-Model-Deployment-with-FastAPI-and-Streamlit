aeabd793c8ea3f51f1f8c749aa9e5fe68e066fb7
Adejumo Ridwan Suleiman <adejumo999@gmail.com>
2024-02-15 17:28:27 +0100

    a

---
+++ 4_streamlit/7_pages/main.py
@@ -0,0 +1,10 @@
+"""
+- create your mainscirp file e.g main.py
+- create a new pages folder
+- each page is a .py files which you can add to your app
+- run streamlit run main_page.py
+"""
+import streamlit as st
+
+st.markdown("# Page 2 ❄️")
+st.sidebar.markdown("# Page 2 ❄️")
