818f5d0b9deae8b6acb5c32c54263f2b01a175f4
Adejumo Ridwan Suleiman <adejumo999@gmail.com>
2024-02-29 17:08:25 +0100

    a

--- 2_fastapi/9_templates/main.py
+++ 2_fastapi/9_templates/main.py
@@ -5,22 +5,15 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/students/{student_id}", response_class=HTMLResponse)
 async def read_student(request: Request, student_id: str):
     return templates.TemplateResponse(
         request=request, name="students.html", context={"student_id": student_id}
-    )
-
-
-@app.get("/numbers/", response_class=HTMLResponse)
-async def get_no(request: Request, no: str = 5):
-    return templates.TemplateResponse(
-        request=request, name="no.html", context={"no": no}
     )
 
 
